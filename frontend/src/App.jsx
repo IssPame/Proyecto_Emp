047e81406c3799481e9123058d278f83c57dd2dc
IssPame19 <issabelpamela4e@gmail.com>
2025-04-22 21:44:04 -0400

    Estructura y herramientas necesarias

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+      <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/registro" element={<Register />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+      </Router>
+  );
+}
+
+export default App;
